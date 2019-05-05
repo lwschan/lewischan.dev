21a767a57550574261240f6cbbd3e494facdb48c
Lewis Chan <yeouw95@gmail.com>
2019-05-05 17:22:14 +0800

    Experiment with prop-types in blog

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,70 +1,89 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    markdownRemark as markdownRemarkPropType,
+    site as sitePropType,
+} from 'src/propTypes';
 import { Link, graphql } from 'gatsby';
 
-import Bio from '../components/Bio';
-import Layout from '../components/Layout';
-import SEO from '../components/SEO';
-import { rhythm, scale } from '../utils/typography';
+import Layout from 'src/components/Layout';
+import SEO from 'src/components/SEO';
+import Bio from 'src/components/Bio';
+import { rhythm, scale } from 'src/utils/typography';
 
-class BlogPostTemplate extends React.Component {
-    render() {
-        const post = this.props.data.markdownRemark;
-        const siteTitle = this.props.data.site.siteMetadata.title;
-        const { previous, next } = this.props.pageContext;
+const propTypes = {
+    data: PropTypes.shape({
+        markdownRemark: markdownRemarkPropType,
+        site: sitePropType.isRequired,
+    }).isRequired,
+    location: PropTypes.shape({
+        pathname: PropTypes.string.isRequired,
+    }).isRequired,
+    pageContext: PropTypes.shape({
+        previous: PropTypes.shape({}),
+        next: PropTypes.shape({}),
+    }).isRequired,
+};
 
-        return (
-            <Layout location={this.props.location} title={siteTitle}>
-                <SEO
-                    title={post.frontmatter.title}
-                    description={post.frontmatter.description || post.excerpt}
-                />
-                <h1>{post.frontmatter.title}</h1>
-                <p
-                    style={{
-                        ...scale(-1 / 5),
-                        display: `block`,
-                        marginBottom: rhythm(1),
-                        marginTop: rhythm(-1),
-                    }}
-                >
-                    {post.frontmatter.date}
-                </p>
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
-                <hr
-                    style={{
-                        marginBottom: rhythm(1),
-                    }}
-                />
-                <Bio />
+function BlogPostTemplate({ data, location, pageContext }) {
+    const post = data.markdownRemark;
+    const siteTitle = data.site.siteMetadata.title;
+    const { previous, next } = pageContext;
 
-                <ul
-                    style={{
-                        display: `flex`,
-                        flexWrap: `wrap`,
-                        justifyContent: `space-between`,
-                        listStyle: `none`,
-                        padding: 0,
-                    }}
-                >
-                    <li>
-                        {previous && (
-                            <Link to={previous.fields.slug} rel="prev">
-                                ← {previous.frontmatter.title}
-                            </Link>
-                        )}
-                    </li>
-                    <li>
-                        {next && (
-                            <Link to={next.fields.slug} rel="next">
-                                {next.frontmatter.title} →
-                            </Link>
-                        )}
-                    </li>
-                </ul>
-            </Layout>
-        );
-    }
+    return (
+        <Layout location={location} title={siteTitle}>
+            <SEO
+                title={post.frontmatter.title}
+                description={post.frontmatter.description || post.excerpt}
+            />
+            <h1>{post.frontmatter.title}</h1>
+            <p
+                style={{
+                    ...scale(-1 / 5),
+                    display: `block`,
+                    marginBottom: rhythm(1),
+                    marginTop: rhythm(-1),
+                }}
+            >
+                {post.frontmatter.date}
+            </p>
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            <hr
+                style={{
+                    marginBottom: rhythm(1),
+                }}
+            />
+            <Bio />
+
+            <ul
+                style={{
+                    display: `flex`,
+                    flexWrap: `wrap`,
+                    justifyContent: `space-between`,
+                    listStyle: `none`,
+                    padding: 0,
+                }}
+            >
+                <li>
+                    {previous && (
+                        <Link to={previous.fields.slug} rel="prev">
+                            ← {previous.frontmatter.title}
+                        </Link>
+                    )}
+                </li>
+                <li>
+                    {next && (
+                        <Link to={next.fields.slug} rel="next">
+                            {next.frontmatter.title} →
+                        </Link>
+                    )}
+                </li>
+            </ul>
+        </Layout>
+    );
 }
+
+BlogPostTemplate.propTypes = propTypes;
 
 export default BlogPostTemplate;
 
