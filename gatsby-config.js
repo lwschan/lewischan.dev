c1cec1b57817536268f3e67de9a3bfe8e069a184
Lewis Chan <yeouw95@gmail.com>
2019-05-12 22:55:41 +0800

    Configured the manifest for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,8 +58,8 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `Gatsby Starter Blog`,
-                short_name: `GatsbyJS`,
+                name: `lewischan.dev`,
+                short_name: `lewischan.dev`,
                 start_url: `/`,
                 background_color: `#ffffff`,
                 theme_color: `#663399`,
