e3ec28996fcb52aae7fa9dfd505cd3c37a9d3a0c
Lewis Chan <yeouw95@gmail.com>
2019-05-22 16:26:27 +0800

    Moved components to folder struc, updated dependencies

--- src/components/Post/Header/index.jsx
+++ src/components/Post/Header/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-    children: PropTypes.shape({}).isRequired,
+    children: PropTypes.oneOfType([PropTypes.string, PropTypes.shape({})])
+        .isRequired,
 };
 
 function Header({ children }) {
--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
@@ -21,7 +14,7 @@ const propTypes = {
     description: PropTypes.string,
     lang: PropTypes.string,
     meta: PropTypes.arrayOf(PropTypes.object),
-    keywords: PropTypes.arrayOf(PropTypes.string),
+    keywords: PropTypes.arrayOf(PropTypes.string).isRequired,
     title: PropTypes.string.isRequired,
 };
 
