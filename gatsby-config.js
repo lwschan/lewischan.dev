90342849300e0e0dff4d806d63e3ea5674144813
John Doe <johndoe@foobar.com>
2022-02-19 22:09:50 +0800

    configure defaultTheme tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      sans: ['Open Sans', 'sans-serif'],
+      sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
     },
   },
   variants: {},
