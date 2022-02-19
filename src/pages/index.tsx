d11694f1cd1a235e0ad6eca6d6bceaa9df42f083
John Doe <johndoe@foobar.com>
2022-02-19 22:01:22 +0800

    add variable font OpenSans

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import Layout from 'components/Layout';
 
 function IndexPage() {
-  return <Layout>Hello World</Layout>;
+  return (
+    <Layout>
+      <h1>Hello World</h1>
+    </Layout>
+  );
 }
 
 export default IndexPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+    },
+  },
   variants: {},
   plugins: [],
 };
