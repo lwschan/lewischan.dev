3cfdaf4621e2a0a9a2db377fa5f92f3d52f5b176
John Doe <johndoe@foobar.com>
2022-02-19 15:42:55 +0800

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,20 @@ module.exports = {
     title: 'lewischan.dev',
     siteUrl: 'https://lewischan.dev',
   },
-  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-root-import'],
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-root-import',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [process.env.GOOGLE_ANALYTICS_TRACKING_ID],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+        },
+      },
+    },
+  ],
   jsxRuntime: 'automatic',
 };
