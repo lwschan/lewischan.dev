0e8f2eab6c2665467739dc2cca973978d7c6c720
John Doe <johndoe@foobar.com>
2022-02-19 11:16:01 +0800

    setup for typescript

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: `lewischan.dev`,
+      siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+
+  ]
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+    </main>
+  )
+}
+
+export default IndexPage
