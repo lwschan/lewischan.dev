e2672f240ac8d71d18664edd466dfcc0e82ed0f4
John Doe <johndoe@foobar.com>
2022-02-19 15:31:01 +0800

    add support for dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: 'lewischan.dev',
