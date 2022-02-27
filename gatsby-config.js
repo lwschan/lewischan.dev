d7261e75e0f800147d39407e7fa22346800b302a
Lewis Chan <yeouw95@gmail.com>
2022-02-27 14:54:55 +0800

    add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify-cms',
   ],
   jsxRuntime: 'automatic',
 };
