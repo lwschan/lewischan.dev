961e92733bd1702d98e3c2dd8b40f7c16c794f5b
Lewis Chan <8966669+lewisgauss@users.noreply.github.com>
2022-05-26 16:50:53 +0000

    move title to shared layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,18 @@
 import { ReactNode } from 'react';
+import { Helmet } from 'react-helmet';
 import Header from './Header';
 
 interface LayoutProps {
+  title: string;
   children: ReactNode;
 }
 
-function Layout({ children }: LayoutProps) {
+function Layout({ title, children }: LayoutProps) {
   return (
     <>
+      <Helmet>
+        <title>{`${title} - lewischan.dev`}</title>
+      </Helmet>
       <Header />
       <div>
         <main className="container py-4">{children}</main>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 import Layout from 'components/Layout';
 
 function About() {
-  return <Layout>This page is a work in progress.</Layout>;
+  return <Layout title="About">This page is a work in progress.</Layout>;
 }
 
 export default About;
